4c6c340e4209a6b94e65a332e3bf7a612b1d61ec
Daniele Cesca <danielecesca@Danieles-MacBook-Air.local>
2022-04-05 11:05:13 +0200

    add basic content structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <p></p>
+        <h1>React and Morty</h1>
       </header>
+      <section class="card">
+        <img
+          class=""
+          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+          alt="Morty Smith"
+        />
+        <p>Morty Smith</p>
+      </section>
+      <nav>
+        <ul>
+          <li>
+          <span class="dot"></span>
+          </li>
+
+          <li>
+          <span class="dot"></span>
+          </li>
+
+          <li>
+          <span class="dot"></span>
+          </li>
+
+          <li>
+          <span class="dot"></span>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
